44dec778bc1d986d55527a8ffccebe84281b2be2
Shayde Nofziger <shaydenofziger@gmail.com>
2017-04-18 11:36:45 -0400

    Add comments explaining what the scripts do

--- bash/getDirectorySize.sh
+++ bash/getDirectorySize.sh
@@ -1,3 +1,5 @@
+# This script is designed to be included in your ~/.bashrc or equivalent file loaded on bash startup.
+# Retrieves the size on disk value in bytes for the working directory.
 alias dirsize=GetDirectorySize
 function GetDirectorySize()
 {
