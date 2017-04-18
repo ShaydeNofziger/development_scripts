06d050de332a24b0a5a4398d49ad6df303a1f03b
Nabil Elqatib <nabilelqatib@gmail.com>
2017-04-18 23:35:30 +0200

    Fix indentation

--- bash/openGitHubProject.sh
+++ bash/openGitHubProject.sh
@@ -17,9 +17,10 @@ function GitHub()
     else
 	    echo "ERROR: Remote origin is invalid" && return false;
     fi
+    
     if [[ uname == 'Linux' ]]; then 
     	xdg-open "${url}"
-	elif [[ uname == 'Darwin' ]]; then
+    elif [[ uname == 'Darwin' ]]; then
 		open "${url}"
     fi
 }
