37716f4198693d1e1cb5a90b885912e073b901ff
Shayde Nofziger <shaydenofziger@gmail.com>
2017-04-18 11:32:47 -0400

    Open GitHub homepage from local project directory

---
+++ bash/openGitHubProject.sh
@@ -0,0 +1,13 @@
+alias github=GitHub
+function GitHub()
+{
+    if [ ! -d .git ] ; 
+        then echo "ERROR: This isnt a git directory" && return false; 
+    fi
+    git_url=`git config --get remote.origin.url`
+    if [[ $git_url != https://github* ]] ;
+        then echo "ERROR: Remote origin is invalid" && return false;
+    fi
+    url=${git_url%.git}
+    open $url
+}
